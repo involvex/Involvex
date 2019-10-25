60834f382e25b09b8365ce6b36fa2b9dccfefb15
involvex <involvex@users.noreply.github.com>
2019-10-25 11:40:30 +0200

    testphp

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <?php echo "phptest"; ?>
   <meta charset="utf-8">
   <title>Involvex's Homepage</title>
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
